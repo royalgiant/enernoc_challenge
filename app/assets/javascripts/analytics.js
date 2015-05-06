320990656cce2f37e0a929cb2f9650ce327aac94
Donald Lee <donaldlee50@gmail.com>
2015-05-06 16:55:36 -0700

    js

---
+++ app/assets/javascripts/analytics.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$('form.import').submit(function(e) {
+	   	var $this = $(this);
+	   	var $input =  $this.find('input#file').val();
+		if($input == 0) {
+		  alert ("Choose a file first!");
+		  return false; 
+		  e.preventDefault(); 
+		}    
+	});  
+});
